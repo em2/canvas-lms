58dbaf5713015e817816714daee1ad8b3dd57ec2
James Carbine <JamesCarbine@gmail.com>
2012-03-03 09:11:01 -0700

    when user clicks generate, checks to make sure all form fields are filled in.

--- app/controllers/id_gatekeepers_controller.rb
+++ app/controllers/id_gatekeepers_controller.rb
@@ -1,7 +1,12 @@
 class IdGatekeepersController < ApplicationController
   def generate
-    #@id_gatekeeper = IdGatekeeper.new
-    
+    if (params[:id_gatekeeper][:instance] == "" ||  params[:id_gatekeeper][:stage] == nil)
+      flash[:notice] = "Please complete the form."
+      redirect_back_or_default(dashboard_url)
+    else
+      @id_gatekeeper = IdGatekeeper.new
+      
+    end
   end
   
   def show
