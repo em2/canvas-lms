96e95b1fe10906039cf286b4a8b6af6fc10abffd
James Carbine <JamesCarbine@gmail.com>
2012-04-25 17:12:16 -0600

    the generation screen is now validating the form data client side with javascript. if the instance or the Class ID's are not in proper formation, it shows red words stating what is wrong and won't let the form be submited.

---
+++ public/javascripts/assign_probe.js
@@ -0,0 +1,73 @@
+
+
+function assignProbeValidator(){
+	
+	$('#rosters_instance').change(function(){
+		validateInstance(this.value);
+	});
+	
+	function validateInstance(text){
+		//
+		// Check for correct length of the instance
+		if (text.length == 3){
+			//
+			// now make sure they are all numbers
+			if (text.match(/[0-9]*/)[0].length == 3){
+				$('#instance_validator').hide();
+				return false;
+			}
+			else{
+				$('#instance_validator').show();
+				return true;
+			}
+		}
+		else{
+			$('#instance_validator').show();
+			return true;
+		}
+	}
+	
+	$('#rosters_courses').change(function(){
+		validateCourses(this.value);
+	});
+	
+	function validateCourses(text){
+		var course_titles = text.split(/[\r\n\t\,\; ]+/);
+		
+		//
+		// loop through each course title and and find any that are not valid
+		for (var i=0; i<course_titles.length; i++){
+			if (course_titles[i].length != 16 || course_titles[i].match(/[Dd][0-9]{3}[Ss][0-9]{3}[Tt][0-9]{3}[Cc][0-9]{3}/) == null){
+				$('#courses_text_validator').show();
+				$('#courses_example_A_validator').show();
+				$('#courses_example_B_validator').show();
+				return true;
+			}
+		}
+		
+		//
+		// if we reach this code, then we are valid and good to go
+		$('#courses_text_validator').hide();
+		$('#courses_example_A_validator').hide();
+		$('#courses_example_B_validator').hide();
+		return false;
+	}
+	
+	$('#rosters_submit').click(function(event){
+		var dirty = true;
+		if (validateInstance($('#rosters_instance').val()) == false){
+			if (validateCourses($('#rosters_courses').val()) == false){
+				dirty = false;
+			}
+		}
+		
+		if (dirty){
+			alert("There are errors on the form. Please fix them.");
+			event.preventDefault();
+			event.stopPropagation();
+			return false;
+		}
+	});
+
+	
+}
