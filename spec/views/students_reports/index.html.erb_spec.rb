a304e3b4ae35097fb197e9b983b995ebbd57e47a
James Carbine <JamesCarbine@gmail.com>
2012-12-17 13:27:07 -0700

    converted tabs to spaces

--- spec/views/students_reports/index.html.erb_spec.rb
+++ spec/views/students_reports/index.html.erb_spec.rb
@@ -2,23 +2,23 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../views_helper')
 
 describe "/students_reports/index" do
-	it "should render" do
+  it "should render" do
     district_account = Account.default.sub_accounts.create!(:name => 'D001')
     school_account = district_account.sub_accounts.create!(:name => 'S001')
-		course_with_teacher_logged_in(:active_all => true, :account => school_account)
-		view_context
+    course_with_teacher_logged_in(:active_all => true, :account => school_account)
+    view_context
 
-		assigns[:quizzes] = [@course.quizzes.create!]
-		assigns[:course] = @course
-		fill_quiz_and_misconceptions
-		assigns[:quiz] = @quiz
+    assigns[:quizzes] = [@course.quizzes.create!]
+    assigns[:course] = @course
+    fill_quiz_and_misconceptions
+    assigns[:quiz] = @quiz
     assigns[:unpublished_quizzes] = []
     assigns[:assignment_quizzes] = assigns[:quizzes]
     assigns[:open_quizzes] = assigns[:quizzes]
     assigns[:surveys] = assigns[:quizzes]
     assigns[:submissions_hash] = {}
     assigns[:misconceptions] = @quiz.quiz_misconceptions
-		assigns[:submissions] = @submissions
+    assigns[:submissions] = @submissions
     assigns[:high_probability] = @high_probability
     assigns[:somewhat_probability] = @somewhat_probability
     assigns[:user_difficulties] = @user_difficulties
