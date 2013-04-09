3fb8d3137a73b476d567966c2d7bc57608ea732a
James Carbine <JamesCarbine@gmail.com>
2013-04-09 10:27:31 -0600

    can now edit misconceptions from the misconception page instead of just through the edit quiz page. same goes for question banks and their misconceptions

--- app/controllers/quiz_misconceptions_controller.rb
+++ app/controllers/quiz_misconceptions_controller.rb
@@ -18,6 +18,31 @@ class QuizMisconceptionsController < ApplicationController
   def update
     if authorized_action(@context, @current_user, :read)
       @misconception = QuizMisconception.find(params[:id])
+      if params[:quiz_misconception][:pattern]
+        pattern = JSON.parse(params[:quiz_misconception][:pattern])
+        quiz = Quiz.find(params[:quiz_id])
+        quiz_data = quiz.quiz_data
+        quiz_data.each do |qd|
+          qd[:answers].each do |qa|
+            a = JSON.parse(qa[:misconception_id])
+            a[params[:id]] = pattern["#{qd[:id]}"]["#{qa[:id]}"]
+            qa[:misconception_id] = a.to_json
+          end
+          quiz_question = QuizQuestion.find(qd[:id])
+          quiz_question[:question_data][:answers].each do |qa|
+            a = JSON.parse(qa[:misconception_id])
+            a[params[:id]] = pattern["#{qd[:id]}"]["#{qa[:id]}"]
+            qa[:misconception_id] = a.to_json
+          end
+          quiz_question.save!
+        end
+        params[:quiz_misconception][:pattern] = pattern
+
+        quiz.generate_quiz_data
+        quiz.published_at = Time.now
+        quiz.workflow_state = 'available'
+        quiz.save!
+      end
       if @misconception.update_attributes(params[:quiz_misconception]) && is_account_admin?
         @misconception.reload
         render :json => @misconception.to_json
