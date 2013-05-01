a324fadd274ded008dbb4b11fae7e191d0a429c5
James Carbine <JamesCarbine@gmail.com>
2013-05-01 17:46:11 -0600

    fixed local_changer to properly find and change the data it is given. added yml merger to merge the new local file with the old

--- lib/locales_changer.rb
+++ lib/locales_changer.rb
@@ -5,16 +5,21 @@ require 'ruby-debug'
 class LocalesChanger
   def initialize(args)
     @source_file = YAML.load_file('config/locales/generated/en.yml')
-    @destination_file = YAML.load_file('config/locales/en_em2.yml')
-    @word_to_change = args[0]
-    @desired_word = args[1]
+    @word_to_change1 = args[0] ? args[0] : ""
+    @desired_word1 = args[1] ? args[1] : ""
+    @word_to_change2 = args[2] ? args[2] : ""
+    @desired_word2 = args[3] ? args[3] : ""
+    @word_to_change3 = args[4] ? args[4] : ""
+    @desired_word3 = args[5] ? args[5] : ""
+    @word_to_change4 = args[6] ? args[6] : ""
+    @desired_word4 = args[7] ? args[7] : ""
     @yml = {}
   end
 
   def run
     @source_file.each do |key, value|
       @yml = self.search(:key => key, :value => value)
-      File.open('asdf.yml', 'w') do |out|
+      File.open('config/locales/asdf.yml', 'w') do |out|
         YAML.dump(@yml, out)
       end
     end
@@ -22,15 +27,34 @@ class LocalesChanger
 
   def search(data={})
     yml = {}
-    if !data[:value].is_a?(Hash) && data[:value] && data[:value].is_a?(String) && data[:value].include?(@word_to_change) && !data[:value].include?("%{")
-      value = data[:value].gsub("#{@word_to_change}", "#{@desired_word}")
-      return value
+    if !data[:value].is_a?(Hash) && data[:value] && data[:value].is_a?(String) && data[:value].include?(@word_to_change1) && !data[:value].include?("%{")
+      value = data[:value].gsub("#{@word_to_change1}", "#{@desired_word1}")
+      return { data[:key] => value }
+    elsif !data[:value].is_a?(Hash) && data[:value] && data[:value].is_a?(String) && data[:value].include?(@word_to_change2) && !data[:value].include?("%{")
+      value = data[:value].gsub("#{@word_to_change2}", "#{@desired_word2}")
+      return { data[:key] => value }
+    elsif !data[:value].is_a?(Hash) && data[:value] && data[:value].is_a?(String) && data[:value].include?(@word_to_change3) && !data[:value].include?("%{")
+      value = data[:value].gsub("#{@word_to_change3}", "#{@desired_word3}")
+      return { data[:key] => value }
+    elsif !data[:value].is_a?(Hash) && data[:value] && data[:value].is_a?(String) && data[:value].include?(@word_to_change4) && !data[:value].include?("%{")
+      value = data[:value].gsub("#{@word_to_change4}", "#{@desired_word4}")
+      return { data[:key] => value }
     elsif data[:value].is_a?(Hash)
       data[:value].each do |key, value|
-        yml[data[:key]] = self.search(:key => key, :value => value)
+        if yml[data[:key]].nil?
+          asdf = self.search(:key => key, :value => value)
+          if asdf.is_a?(Hash)
+            yml[data[:key]] = asdf
+          end
+        else
+          asdf = self.search(:key => key, :value => value)
+          if asdf.is_a?(Hash)
+            yml[data[:key]].merge!(asdf)
+          end
+        end
       end
     end
-    yml
+    yml.empty? ? "" : yml
   end
 end
 
