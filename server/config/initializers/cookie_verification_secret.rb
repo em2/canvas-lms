accc96d61f89d82d3b078019f0539e8f0c00979e
Cole Busby <colebusby@gmail.com>
2014-06-16 11:57:05 -0600

    added calculate report button

---
+++ server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '91b328240a5bf98c2942ae62aec4af22d490f033aba28c931363b8b9e97122e3e3222206a7a33d2e45fa520ea0650b7731ae6c1777845afc637d26f74c75e561';
