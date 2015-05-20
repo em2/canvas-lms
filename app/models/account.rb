e2134b25e4335112919063709caf3c3c9bb0da8b
Cole Busby <colebusby@gmail.com>
2015-05-20 09:52:41 -0600

    permissions tab issues

--- app/models/account.rb
+++ app/models/account.rb
@@ -755,7 +755,7 @@ class Account < ActiveRecord::Base
 
   TAB_COURSES = 0
   TAB_STATISTICS = 1
-  TAB_PERMISSIONS = 2
+  # TAB_PERMISSIONS = 2
   TAB_SUB_ACCOUNTS = 3
   TAB_TERMS = 4
   TAB_AUTHENTICATION = 5
@@ -798,6 +798,7 @@ class Account < ActiveRecord::Base
       # tabs << { :id => TAB_ROSTERS, :label => t('#account.tab_rosters', "Rosters"), :css_class => 'rosters', :href => :rosters_path, :no_args => true } if user && self.grants_right?(user, nil, :manage_grades)
       tabs << { :id => TAB_USERS, :label => t('#account.tab_users', "Users"), :css_class => 'users', :href => :account_users_path } if user && self.grants_right?(user, nil, :read_roster)
       # tabs << { :id => TAB_STATISTICS, :label => t('#account.tab_statistics', "Statistics"), :css_class => 'statistics', :href => :statistics_account_path } if user && self.grants_right?(user, nil, :view_statistics)
+      # tabs << { :id => TAB_PERMISSIONS, :label => t('#account.tab_permissions', "Permissions"), :css_class => 'permissions', :href => :account_permissions_path } if user && self.grants_right?(user, nil, :manage_role_overrides)
       if user && self.grants_right?(user, nil, :manage_outcomes)
         # tabs << { :id => TAB_OUTCOMES, :label => t('#account.tab_outcomes', "Outcomes"), :css_class => 'outcomes', :href => :account_outcomes_path }
         # tabs << { :id => TAB_RUBRICS, :label => t('#account.tab_rubrics', "Rubrics"), :css_class => 'rubrics', :href => :account_rubrics_path }
