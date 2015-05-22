92725dafa6d8980bb464a4729f9c4f8f1044cef0
Cole Busby <colebusby@gmail.com>
2015-05-21 18:20:01 -0600

    switching add assess to delayed job

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -65,7 +65,7 @@ class RostersController < ApplicationController
     # Make sure that the stage was selected
     if check_stage(params[:rosters][:stage])
       errors_found = true
-    endz
+    end
 
     #
     # Make sure that the instance is 3 numbers and only 3 numbers
@@ -143,9 +143,9 @@ class RostersController < ApplicationController
 
             #
             # Send off the roster to generate everything to delayed_job
-            # Delayed::Job.enqueue(RosterGenerateJob.new(@roster, @context, @probe, @instance, @stage, @course_title, @current_user, @number_students, @district, @district_account, @school_account, @teacher, @pre_post))
+            Delayed::Job.enqueue(RosterGenerateJob.new(@roster, @context, @probe, @instance, @stage, @course_title, @current_user, @number_students, @district, @district_account, @school_account, @teacher, @pre_post))
             #@roster.send_later(:generate_probes, @context, @probe, @instance, @stage, @course_title, @current_user, @number_students, @district, @district_account, @school_account, @teacher, @pre_post)
-            @roster.generate_probes(@context, @probe, @instance, @stage, @course_title, @current_user, @number_students, @district, @district_account, @school_account, @teacher, @pre_post, @course.id)
+            # @roster.generate_probes(@context, @probe, @instance, @stage, @course_title, @current_user, @number_students, @district, @district_account, @school_account, @teacher, @pre_post, @course.id)
 
             probe_generated = true
           else
